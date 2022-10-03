595987c18427e3c860db6c758dd76c5f8b8ea505
Hung Tran <Hung_Tran@epam.com>
2022-10-03 13:41:28 +0700

    remove codes

--- src/redis.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import redis from 'redis';
-
-const redisClient = redis.createClient({
-  url: process.env.REDIS_URL,
-  password: process.env.REDIS_PASSWORD,
-});
-
-export default redisClient;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,4 @@
 export * from './customsError';
 export * from './validateFunctions';
-export * from './redisActions';
-export * from './generateToken';
 export * from './pagination';
 export * from './userUtils';

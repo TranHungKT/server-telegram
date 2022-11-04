3cd5e9e5f848cbe9cfd56014fbf41696baf01ffd
Hung Tran <Hung_Tran@epam.com>
2022-11-04 16:40:15 +0700

    refactor

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { fileRouters } from './fileRouters';
 import { groupRouter } from './groupRouters';
 import { messageRouter } from './messageRouters';
 import { userRouter } from './userRouters';
@@ -10,5 +11,6 @@ const router = Router();
 router.use(userRouter);
 router.use(groupRouter);
 router.use(messageRouter);
+router.use(fileRouters);
 
 export { router };

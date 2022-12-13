2f700718bad1584cae16cf95dd8fa3dd8de9809e
Hung Tran <Hung_Tran@epam.com>
2022-12-13 15:01:56 +0700

    fixed warning

--- src/constants/listOfSocketEvents.ts
+++ src/constants/listOfSocketEvents.ts
@@ -8,6 +8,9 @@ export const SOCKET_EVENTS = {
   SOCKET_ERROR: 'socket-error',
   TYPING: 'typing',
   UN_TYPING: 'un-typing',
+  OFFER_FOR_CALL_EVENT: 'offer-for-call-event',
+  ANSWER_FOR_CALL_EVENT: 'answer-for-call-event',
+  ICE_CANDIDATE_EVENT: 'ice-candidate-event',
 };
 
 export const SOCKET_CONNECT_USERS = 'SOCKET_CONNECT_USERS';
